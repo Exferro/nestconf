eb686f1c82017218794b46ce70fb0784434faa9e
Aleksei Malyshev <aleksei.o.malyshev@gmail.com>
2025-03-27 16:56:51 +0100

    Add meta allowing one to combine Configurable and ABC

--- nestconf/__init__.py
+++ nestconf/__init__.py
@@ -1,2 +1,8 @@
 from .config import Config
 from .configurable import Configurable, ConfigurableMeta
+
+from abc import ABCMeta
+
+class ConfigurableABCMeta(ConfigurableMeta, ABCMeta):
+    """Metaclass that combines ConfigurableMeta and ABCMeta"""
+    pass
